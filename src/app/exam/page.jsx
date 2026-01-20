518faa46409ac3063b1b81a940ac141d766452d2
epenagandlavenkatanarayana-droid <narayanaepenagandla@gmail.com>
2026-01-20 20:10:41 +0530

    changed footer at exam panel

--- src/app/exam/page.jsx
+++ src/app/exam/page.jsx
@@ -129,7 +129,10 @@ export default function ExamPage() {
           </div>
         </div>
       </ExamProvider>
-      <Footer />
+      <div className="flex p-2 gap-2 mt-5 text-big  justify-center">
+  <span>&copy;</span> 
+  Copyright 2025 Charani Infotech Pvt Ltd. All Rights Reserved.
+</div>
     </ExamLayout>
   );
 }
